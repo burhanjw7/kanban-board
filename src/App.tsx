ac5c60851ffd14035fa79a31524fbdc0a994da28
burhanjw7 <burhanjw7@gmail.com>
2021-02-01 17:09:32 +0530

    Application skeleton with static data

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@material-ui/core";
+import React from "react";
+
+
+const App: React.FC = (): JSX.Element => (
+  <Container maxWidth="md">
+    <h1> My Kanban Board </h1>
+  </Container>
+);
+export default App;
