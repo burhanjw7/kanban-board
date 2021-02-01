db45271bdd28c155fa2ed6b4d44adf5491f7dcb5
burhanjw7 <burhanjw7@gmail.com>
2021-02-01 19:18:53 +0530

    Add in progress board and start task function

--- src/KanbanBoard/index.tsx
+++ src/KanbanBoard/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import BoardBox from "../Components/BoardBox";
 import ToDoBoard from "../Components/Boards/ToDoBoard";
 import { ITask } from "../Components/BoardItemBox";
+import InProgressBoard from "../Components/Boards/InProgressBoard";
 
 import moment from "moment";
 
@@ -16,13 +17,22 @@ const useStyles = makeStyles((theme) =>
 
 const KanbanBoard: React.FC = (): JSX.Element => {
   const classes = useStyles();
+  
   const [boardData, setBoardData] = useState<ITask[]>([]);
+  
   const addTask = (task: ITask) => {
     const tempBoardData = Object.assign([], boardData);
     tempBoardData.push(task);
     setBoardData(tempBoardData);
   };
   
+  const startTask = (taskId: string) => {
+    const tempBoardData: ITask[] = Object.assign([], boardData);
+    const index = tempBoardData.findIndex((board) => board.id === taskId);
+    tempBoardData[index].status = "in-progress";
+    tempBoardData[index].startTime = moment();
+    setBoardData(tempBoardData);
+  };
   
   return (
     <>
@@ -37,12 +47,16 @@ const KanbanBoard: React.FC = (): JSX.Element => {
             <ToDoBoard
               onCreate={addTask}
               tasks={boardData.filter((data) => data.status === "todo")}
-              onStart={()=>{}}
+              onStart={startTask}
             />
           </BoardBox>
         </Grid>
         <Grid item sm={4}>
           <BoardBox title={"In Progress"}>
+            <InProgressBoard
+              tasks={boardData.filter((data) => data.status === "in-progress")}
+              onResolve={() => {}}
+            />
           </BoardBox>
         </Grid>
         <Grid item sm={4}>
